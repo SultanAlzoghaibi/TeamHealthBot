3a3194a2b62c37a3f02350a074aa6284cb9b4eae
30178597 <sultan.amrani@ucalgary.ca>
2025-04-20 19:08:43 -0600

    v1.00: Setup Spring Boot Maven

--- src/main/java/com/teamheath/bot/TeamHealthBotApplication.java
+++ src/main/java/com/teamheath/bot/TeamHealthBotApplication.java
@@ -2,7 +2,7 @@ package com.teamheath.bot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+// commit test
 @SpringBootApplication
 public class TeamHealthBotApplication {
 
