4727f9b1ea7cb84c2a4641a7e828999550124374
30178597 <sultan.amrani@ucalgary.ca>
2025-05-13 16:41:04 -0700

    v1.18 gRPC: working gRPC to C++ microservice and back

---
+++ src/main/java/com/teamheath/bot/GrpcClientConfig.java
@@ -0,0 +1,24 @@
+package com.teamheath.bot;
+
+import com.teamhealth.grpc.ScoreServiceGrpc;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GrpcClientConfig {
+
+    @Bean
+    public ManagedChannel grpcChannel() {
+        return ManagedChannelBuilder
+                .forAddress("localhost", 50051)
+                .usePlaintext()
+                .build();
+    }
+
+    @Bean
+    public ScoreServiceGrpc.ScoreServiceBlockingStub scoreServiceStub(ManagedChannel grpcChannel) {
+        return ScoreServiceGrpc.newBlockingStub(grpcChannel);
+    }
+}
