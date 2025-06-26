5e8517196892173f3aaf4f76e3fcfdf165ba9322
sultanalzoghaibi <ssultan007@icloud.com>
2025-06-26 13:51:21 -0700

    v1.38 CMD: register join dub checker and register del added (later change it so you wont need password)

--- src/main/java/com/teamheath/bot/Commands/Users/User/UserRepository.java
+++ src/main/java/com/teamheath/bot/Commands/Users/User/UserRepository.java
@@ -2,7 +2,9 @@ package com.teamheath.bot.Commands.Users.User;
 
 import com.teamheath.bot.Commands.Users.Org.OrgDB;
 import com.teamheath.bot.Commands.Users.Team.TeamDB;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -13,6 +15,9 @@ public interface UserRepository extends JpaRepository<UserDB, UUID> {
     Optional<Object> findWithTeamBySlackUserId(String userId);
 
     int countByTeam(TeamDB team);
+
+    @Query("SELECT u FROM UserDB u WHERE u.slackUserId = :slackUserId AND u.organization.id = :orgId")
+    Optional<UserDB> findBySlackUserIdAndOrgId(@Param("slackUserId") String slackUserId, @Param("orgId") UUID orgId);
 
     Optional<UserDB> findBySlackUserIdAndOrganization(String userId, OrgDB org);
 }
